c9d06f8ae3f114504c0aa3536f20f614cda51c3d
TrifonEstov <don.tito.g@gmail.com>
2017-01-31 21:40:00 +0200

    New program added. If the program uses Arrays, Judge returns Exception!

---
+++ DaysOfWeek/DaysOfWeek.cs
@@ -0,0 +1,49 @@
+﻿namespace DaysOfWeek
+{
+    using System;
+
+    public class DaysOfWeek
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            //Console.WriteLine(Day(n));
+            switch(n)
+            {
+                case 1: Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default: Console.WriteLine("Invalid day!");
+                    break;
+            }
+        }
+
+        //private static string Day(int n)
+        //{
+        //    if (n < 0 || n > 7)
+        //    {
+        //        return "Invalid day!";
+        //    }
+        //    string[] days = {"Invalid day!", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+        //    "Saturday", "Sunday"};
+        //    return Day(n);
+        //}
+    }
+}
