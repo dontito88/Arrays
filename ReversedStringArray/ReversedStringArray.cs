5f7500a27a4e8979ceaf42ceaef74fd7a01585fd
TrifonEstov <don.tito.g@gmail.com>
2017-02-04 17:18:33 +0200

    New program added!

---
+++ ReversedStringArray/ReversedStringArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedStringArray
+{
+    class ReversedStringArray
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            Array.Reverse(input);
+            foreach (var item in input)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
