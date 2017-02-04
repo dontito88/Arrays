9ab1094de723da26daf1bcf09da7f3f20f8e48f5
TrifonEstov <don.tito.g@gmail.com>
2017-02-04 16:48:25 +0200

    New program added!

---
+++ RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundingNumbers
+{
+    class RoundingNumbers
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().Trim();
+            var numbers = input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            double[] array = new double[numbers.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = double.Parse(numbers[i]);
+
+                Console.WriteLine("{0} => {1}", array[i], Math.Round(array[i], MidpointRounding.AwayFromZero));
+            }
+        }
+    }
+}
